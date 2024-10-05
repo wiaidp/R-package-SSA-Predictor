33f6e1277a5e01c640cdc8a56057be49fadceced
marc <marcadolfrudolf.wildi@gmail.com>
2024-10-05 11:33:19 +0200

    Update tutorial_5_maximal_monotone_nowcast.R

--- SSA Tutorials/tutorial_5_maximal_monotone_nowcast.R
+++ SSA Tutorials/tutorial_5_maximal_monotone_nowcast.R
@@ -12,8 +12,13 @@
 #   -In its dual form: the predictor generates the least number of zero-crossings in first differences.
 #   -Equivalently: the original predictor (in levels) is maximal monotone
 
-# Application: we compute a maximal monotone SSA nowcast for the two-sided (bi-infinite) HP(14400) applied to monthly US-INDPRO.
-
+# Application/prediction problem:
+# -We compute a maximal monotone SSA nowcast for the two-sided (bi-infinite) HP(14400) applied to monthly US-INDPRO.
+# -We compute the MSE nowcast of HP based on an ARIMA(1,1,0)-model for INDPRO
+# -This MSE nowcast is much noisier than the classic HP concurrent (real-time) filter, widely used in business-cycle applications
+# -Therefore, we replicate the holding-time (smoothness) of the classic one-sided HP by SSA
+# -By optimization we then expect SSA to equal classic HP in terms of smoothness and to outperform classic HP in terms of MSE (tracking accuracy)
+# -Since SSA is much smoother than the MSE nowcast we are interested in finding out how much SSA looses in terms of MSE performances.
 
 #-----------------------------------------------------------------------
 # Make a clean-sheet, load packages and functions
@@ -102,54 +107,60 @@ Xi_tilde=filter_obj$Xi_tilde
 Sigma=filter_obj$Sigma
 # Inverse of integration, see section 5.3 Wildi (2025)
 Delta=filter_obj$Delta
-# Wold decomposition: depends on a1
+# Wold decomposition (MA-inversion of AR(1) with parameter a1=0.3): depends on a1
 Xi=filter_obj$Xi
 # Two-sided target: used for computing sample MSEs below
 hp_two=filter_obj$hp_two
-# Classic concurrent HP
+# Classic concurrent HP: we want to replivate the latter's holding-time by SSA
 hp_trend=filter_obj$hp_trend
 
 # Specify rho1 in HT constraint, see Wildi (2025) section 5.4
-# 1. Compute rho_mse
-#   Use MA-inversion, i.e., gamma_mse=Xi%*%gamma: this is applied to stationary first differences 
+# 1. Compute rho_mse the lag-one ACF of the MSE nowcast
+#   The holding time constraint is expressed in (stationary) first differences of the I(1) process
+#   The (differenced) data is an AR(1) and therefore we have to compute the convolution of filters and MA-inversion, see tutorial 2
 rho_mse<-compute_holding_time_func(Xi%*%gamma_mse)$rho_ff1
 # We also use the MA-inversion Xi%*%hp_trend for deriving the lag-one ACF of HP-C
 rho_hp_concurrent<-as.double(compute_holding_time_func(Xi%*%hp_trend)$rho_ff1)
 
+# The lag-one ACF of the MSE nowcast is small: the filter generates loss of noisy zero-crossings
 rho_mse
-# Set rho1=rho_hp_concurrent in HT constraint
+# We set rho1=rho_hp_concurrent in the HT constraint
 # Research question/assumption: impose same smoothness as HP-C; should outperform HP-C in terms of MSE; without loosing too much vs. gamma_mse 
 rho1<-rho_hp_concurrent
 
 
 #----------------------------
-# Compute SSA solution
-#   Use optim to determine optimal lambda numerically
+# Compute SSA solution, seee Wildi (2025) sections 5.3 and 5.4
+#   Use optim to determine optimal Lagrangian multiplier lambda numerically
+#     The optimal Lagrangian ensures compliance with the HT constraint
 #   Initialize lambda with 0: MSE benchmark (numerical optimization must improve upon MSE-initialization)
 lambda<-0
 opt_obj<-optim(lambda,b_optim,lambda,gamma_mse,Xi,Sigma,Xi_tilde,M,B,gamma_tilde)
 
+# Optimized lambda
 lambda_opt<-opt_obj$par
 
 # Compute cointegrated I(1) SSA solution for the optimal Lagrangian parameter lambda_opt
 bk_obj<-bk_int_func(lambda_opt,gamma_mse,Xi,Sigma,Xi_tilde,M,B,gamma_tilde)
 
-# Lag-one ACF: should match HT constraint
+# Lag-one ACF: matches HT constraint rho1 as desired
 bk_obj$rho_yy
-# Correlation with target (based on synthetic stationary series)
+rho1
+# Correlation with target (based on synthetic stationary series, see Wildi (2025), section 5.3)
 bk_obj$rho_yz
 # MSE: with respect to causal MSE predictor
 bk_obj$mse_yz
-# b applied to x
+# SSA filter: b applied to x, i.e., INDPRO
 b_x<-bk_obj$b_x
 # b applied to eps in synthetic series
 b_eps<-bk_obj$b_eps
 
 # If Xi=Identity then b_x=b_eps
-ts.plot(b_eps)
-ts.plot(b_x)
+par(mfrow=c(1,2))
+ts.plot(b_eps,main="B as applied to epsilon")
+ts.plot(b_x,main="b as applied to INDPRO")
 
-# Check cointegration constraint: difference should vanish
+# Check cointegration constraint: difference should vanish, see Wildi (2025) section 5.3
 sum(b_x)-sum(gamma_mse)
 # Check HT constraint: difference should vanish for optimal Lagrangian lambda_opt (up to rounding error)
 bk_obj$rho_yy-rho1
@@ -186,13 +197,8 @@ axis(2)
 box()
 
 
-
-if (F)
-{
-  mplot<-cbind(hp_two,c(gamma_mse,rep(0,L-1)),c(b_x,rep(0,L-1)),c(hp_trend,rep(0,L-1)))
-  as_obj<-amp_shift_func(600,mplot[,4],F)
-}
-
+#----------------------------------------
+# Filter data
 
 y_ssa<-filter(x_tilde,b_x,side=1)
 y_hp_concurrent<-filter(x_tilde,hp_trend,side=1)
@@ -200,6 +206,10 @@ y_mse<-filter(x_tilde,gamma_mse,side=1)
 y_target<-filter(x_tilde,hp_two,side=2)
 colo<-c("black","violet","green","blue","red")
 
+# Plots
+# First plot: data in levels
+#   -Classic HP (HP-C) over/under-shoots at peaks and dips; it also systematically lags (right shifted); 
+#   -MSE is closest to target, by design; SSA is as smooth as HP-C but closer to target
 par(mfrow=c(1,1))
 anf<-L+100
 enf<-length(x_tilde)
@@ -216,7 +226,9 @@ axis(2)
 box()
 
 
-
+# Second plot: data in differences
+#   -MSE (top blot) is very noisy and generates lots of zero-crossings
+#   -HP-C and SSA are much smoother and both track last three recessions quite well with few false alarms during longer expansions
 anf<-L+100
 enf<-length(x_tilde)-1
 mplot<-apply(cbind(x_tilde,y_target,y_mse,y_ssa,y_hp_concurrent),2,diff)[anf:enf,]
@@ -269,8 +281,8 @@ axis(2)
 box()
 
 
-
-
+#----------------------------------------
+# Sample performances
 
 # MSE with respect to two-sided target
 mean((y_target-y_mse)^2,na.rm=T)
@@ -279,22 +291,16 @@ mean((y_target-y_hp_concurrent)^2,na.rm=T)
 # MSE with of SSA with respect to one-sided MSE predcitor (this should match bk_obj$mse_yz)
 mean((y_mse-y_ssa)^2,na.rm=T)
 bk_obj$mse_yz
-
 # Compute and compare empirical and theoretical holding times
 # The holding times concern first differences of the series
 compute_empirical_ht_func(diff(y_mse)[anf:enf])$empirical_ht
 compute_holding_time_from_rho_func(rho_mse)$ht
-
 compute_empirical_ht_func(diff(y_ssa)[anf:enf])$empirical_ht
 compute_holding_time_from_rho_func(bk_obj$rho_yy)$ht
-
 compute_empirical_ht_func(diff(y_hp_concurrent)[anf:enf])$empirical_ht
 compute_holding_time_from_rho_func(rho_hp_concurrent)$ht
-
-
 # Target
 compute_empirical_ht_func(diff(y_target))$empirical_ht
-
 
 mat_perf<-matrix(nrow=2,ncol=3)
 mat_perf[1,]<-c(mean((y_target-y_mse)^2,na.rm=T),mean((y_target-y_ssa)^2,na.rm=T),mean((y_target-y_hp_concurrent)^2,na.rm=T))
@@ -302,4 +308,8 @@ mat_perf[2,]<-c(compute_empirical_ht_func(diff(y_mse)[anf:enf])$empirical_ht,com
 colnames(mat_perf)<-c("MSE","SSA","HP-C")
 rownames(mat_perf)<-c("Sample mean square error","Sample holding time")
 
+# MSE-nowcast has smallest MSE, by design; but it is very noisy (small holding-time)
+# HP-C is much smoother but its MSE is twice as large as MSE-nowcast
+# SSA is as smooth as HP-C and its MSE is halfway between MSE-nowcast and HP-C
+# We conclude that SSA can gain in smoothness over MSE without lossing too much in terms of MSE: optimal tradeoff by SSA criterion 
 mat_perf
