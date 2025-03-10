40c0feed10f77a270f06130a3b437178b6fc8654
marc <marcadolfrudolf.wildi@gmail.com>
2025-03-10 17:05:17 +0100

    up

---
+++ SSA Tutorials/tutorial_7.0_introduction_multivariate_SSA_MSSA.R
@@ -0,0 +1,7 @@
+# We here introduce to the M-SSA. see paper provide link???
+
+# Example 1: we replicate the univariate SSA by M-SSA
+
+# Example 2: we illustrate a bivariate M-SSA based on a `bogus' design 
+#  -In principle the same as example 1 but we add an irrelevant (cross-sectionally uncorrelated white noise) series 
+
